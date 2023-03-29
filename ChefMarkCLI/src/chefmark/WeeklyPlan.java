acbe33ac0b2c0e728ad94adaf49d954972290f8a
Msheph1 <max.r.shep@gmail.com>
2023-03-29 18:06:30 -0500

    Max's changes to weekly plan

--- ChefMarkCLI/src/chefmark/WeeklyPlan.java
+++ ChefMarkCLI/src/chefmark/WeeklyPlan.java
@@ -1,5 +1,11 @@
 package chefmark;
 
+import java.util.HashMap;
+import java.util.ArrayList;
+
 public class WeeklyPlan {
 
+
+    private String name;
+    private HashMap<String, ArrayList<Recipe>> weeklyPlan;
 }
