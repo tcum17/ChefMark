b20b3ca7de87e00a92db76e0b12b69ce10441ca7
Tyler <tscummi@ilstu.edu>
2023-04-03 17:30:54 -0500

    Created RecipeDescriptors class

--- ChefMarkCLI/src/chefmark/RecipeDescriptors.java
+++ ChefMarkCLI/src/chefmark/RecipeDescriptors.java
@@ -1,4 +1,26 @@
 package chefmark;
+
+import java.util.ArrayList;
+
 public class RecipeDescriptors {
-    
+    private ArrayList<String> tags;
+    private ArrayList<String> dishType;
+    private ArrayList<String> mealType;
+    private ArrayList<String> cuisineType;
+
+    public ArrayList<String> getTags() {
+        return this.tags;
+    }
+
+    public ArrayList<String> getDishType() {
+        return this.dishType;
+    }
+
+    public ArrayList<String> getMealType() {
+        return this.mealType;
+    }
+
+    public ArrayList<String> getCuisineType() {
+        return this.cuisineType;
+    }
 }
