fddd332c6eb4242c1b457e5ac3c90fbb8aafe21a
Msheph1 <max.r.shep@gmail.com>
2023-04-03 18:04:03 -0500

    DBQuery

--- ChefMarkCLI/src/chefmark/DBQuery.java
+++ ChefMarkCLI/src/chefmark/DBQuery.java
@@ -1,4 +1,11 @@
 package chefmark;
 public class DBQuery {
-    
+    private DBConnection dbconnection;
+    private Connection connection;
+    private Statement statement;
+
+    public ResultSet dbQuery(String query)
+    {
+        
+    }
 }
