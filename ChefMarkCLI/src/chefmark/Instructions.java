22b60e5558eb35694cd04284af02734089a785e3
jake <jacobnothvogel@gmail.com>
2023-04-03 18:26:06 -0500

    instructions

--- ChefMarkCLI/src/chefmark/Instructions.java
+++ ChefMarkCLI/src/chefmark/Instructions.java
@@ -1,4 +1,16 @@
 package chefmark;
 public class Instructions {
     
+    private String instructions;
+    private String cautions;
+
+    public void setInstructions(String instructions)
+    {
+        this.instruction = instructions;
+    }
+
+    public void setCautions(String cautions)
+    {
+        this.cautions = cautions;
+    }
 }
