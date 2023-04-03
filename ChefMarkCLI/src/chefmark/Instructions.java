5b56eac4f3241c0390285963ce32e408cb3d8d0f
Maximum H Wilson <Maximum@Maxs-Macbook-Air-2.local>
2023-04-03 17:12:19 -0500

    testing

--- ChefMarkCLI/src/chefmark/Instructions.java
+++ ChefMarkCLI/src/chefmark/Instructions.java
@@ -1,4 +1,13 @@
 package chefmark;
 public class Instructions {
-    
+    String[] instructions;
+    String[] cautions;
+
+
+    public String[] getInstructions(){
+        return instructions;
+    }
+    public String[] getCautions(){
+        return cautions;
+    }
 }
