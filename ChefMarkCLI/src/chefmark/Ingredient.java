d4f22c4331ba708c65916d7b4a9a7120f79b04ef
Tyler <tscummi@ilstu.edu>
2023-04-03 17:53:08 -0500

    Fixed various errors

--- ChefMarkCLI/src/chefmark/Ingredient.java
+++ ChefMarkCLI/src/chefmark/Ingredient.java
@@ -4,7 +4,6 @@ public class Ingredient {
     private String ingredientName;
     private int ingredientID;
     private float quantity;
-    // NOTE: we don't know all of the measures that API returns
     private String measure;
     private float weight;
     private String foodCategory;
--- ChefMarkCLI/src/chefmark/NutritionalFacts.java
+++ ChefMarkCLI/src/chefmark/NutritionalFacts.java
@@ -1,50 +1,44 @@
 package chefmark;
 
+import java.util.ArrayList;
+
 public class NutritionalFacts {
-    
+
     private ArrayList<String> healthLables;
     private ArrayList<String> dietLables;
     private int calories;
     private int glycemicIndex;
     private int yeild;
 
-    public ArrayList<String> getHealthLables()
-    {
+    public ArrayList<String> getHealthLables() {
         return healthLables;
     }
 
-    public ArrayList<String> getDietLables()
-    {
+    public ArrayList<String> getDietLables() {
         return dietLables;
     }
 
-    public void setCalories(int calories)
-    {
+    public void setCalories(int calories) {
         this.calories = calories;
     }
 
-    public int getCalories()
-    {
+    public int getCalories() {
         returns calories;
     }
 
-    public void setGlycemicIndex(int glycemicIndex)
-    {
+    public void setGlycemicIndex(int glycemicIndex) {
         this.glycemicIndex = glycemicIndex;
     }
 
-    public int getGlycemicIndex()
-    {
+    public int getGlycemicIndex() {
         return glycemicIndex;
     }
 
-    public void setYeild(int yeild)
-    {
+    public void setYeild(int yeild) {
         this.yeild = yeild;
     }
 
-    public int getYeild()
-    {
+    public int getYeild() {
         return yeild;
     }
 }
