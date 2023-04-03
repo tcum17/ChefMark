34bec97e9d56587fc8eb5872fed0c4c06f68710d
jake <jacobnothvogel@gmail.com>
2023-04-03 17:47:23 -0500

    NutritionalFacts

--- ChefMarkCLI/src/chefmark/NutritionalFacts.java
+++ ChefMarkCLI/src/chefmark/NutritionalFacts.java
@@ -1,4 +1,50 @@
 package chefmark;
+
 public class NutritionalFacts {
     
+    private ArrayList<String> healthLables;
+    private ArrayList<String> dietLables;
+    private int calories;
+    private int glycemicIndex;
+    private int yeild;
+
+    public ArrayList<String> getHealthLables()
+    {
+        return healthLables;
+    }
+
+    public ArrayList<String> getDietLables()
+    {
+        return dietLables;
+    }
+
+    public void setCalories(int calories)
+    {
+        this.calories = calories;
+    }
+
+    public int getCalories()
+    {
+        returns calories;
+    }
+
+    public void setGlycemicIndex(int glycemicIndex)
+    {
+        this.glycemicIndex = glycemicIndex;
+    }
+
+    public int getGlycemicIndex()
+    {
+        return glycemicIndex;
+    }
+
+    public void setYeild(int yeild)
+    {
+        this.yeild = yeild;
+    }
+
+    public int getYeild()
+    {
+        return yeild;
+    }
 }
