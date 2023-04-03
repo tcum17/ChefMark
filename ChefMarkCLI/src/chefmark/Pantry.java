861ea0517eec1b979163ec9ff26add449ca6b29d
Tyler <tscummi@ilstu.edu>
2023-04-03 17:49:10 -0500

    Added constructors to RecipeDescriptors class

--- ChefMarkCLI/src/chefmark/Pantry.java
+++ ChefMarkCLI/src/chefmark/Pantry.java
@@ -1,4 +1,17 @@
 package chefmark;
+
+import java.util.ArrayList;
+
 public class Pantry {
-    
+    private ArrayList<Ingredient> ingredientList;
+
+    public ArrayList<Ingredient> getIngredientList() {
+        return this.ingredientList;
+    }
+    public boolean removeIngredient(Ingredient ingredient) {
+
+        
+
+        return false;
+    }
 }
