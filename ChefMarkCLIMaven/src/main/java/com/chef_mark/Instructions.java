b3853e057d018abd82e8fe18b7cdc5e70635a701
Collin <cnelson3014@gmail.com>
2023-04-05 16:21:45 -0500

    Adding classes to maven ver

---
+++ ChefMarkCLIMaven/src/main/java/com/chef_mark/Instructions.java
@@ -0,0 +1,17 @@
+package com.chef_mark;
+
+public class Instructions {
+    
+    private String instructions;
+    private String cautions;
+
+    public void setInstructions(String instructions)
+    {
+        this.instructions = instructions;
+    }
+
+    public void setCautions(String cautions)
+    {
+        this.cautions = cautions;
+    }
+}
---
+++ ChefMarkCLIMaven/src/main/java/com/chef_mark/WeeklyPlan.java
@@ -0,0 +1,43 @@
+package com.chef_mark;
+
+import java.util.HashMap;
+import java.util.ArrayList;
+
+public class WeeklyPlan {
+
+
+    private String name;
+    private HashMap<String, ArrayList<Recipe>> weeklyPlan;
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public void setWeeklyPlan(HashMap<String, ArrayList<Recipe>> weeklyPlan)
+    {
+        this.weeklyPlan = weeklyPlan;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public HashMap<String, ArrayList<Recipe>> getWeeklyPlan()
+    {
+        return this.weeklyPlan;
+    }
+
+    public boolean addRecipeToWeeklyPlan(Recipe recipe)
+    {
+        return true;
+    }
+
+    public boolean removeRecipeFromWeeklyPlan(Recipe recipe)
+    {
+        return true;
+    }
+
+
+}
