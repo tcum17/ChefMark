820cb00934f227d8cda1a462049c41fe62a9a2f1
Tyler <tscummi@ilstu.edu>
2023-04-17 18:30:28 -0500

    Added stuff from 4/17 session

--- ChefMarkCLI/src/chefmark/UserController.java
+++ ChefMarkCLI/src/chefmark/UserController.java
@@ -1,28 +1,32 @@
 package chefmark;
+
+import java.sql.ResultSet;
+
 public class UserController {
     private User user;
 
     public boolean createUser(String username, String password, String email) {
-        //if (check if username and email already exist) -- return true if successfully created, false if otherwise
+        // if (check if username and email already exist) -- return true if successfully
+        // created, false if otherwise
         this.user = new User(username, password, email);
         return true;
     }
 
     public boolean changePassword(String password) {
-        //check if password is valid, false if it can't be changed
+        // check if password is valid, false if it can't be changed
         this.user.setPassword(password);
         return true;
     }
 
     public boolean changeEmail(String email) {
-        //check if email is valid, false if it can't be changed
+        // check if email is valid, false if it can't be changed
         this.user.setEmail(email);
         return true;
     }
 
     public boolean deleteUser(String username) {
-        //find username from ArrayList of users, remove them
-        //return false if it can't be found/deleted, true otherwise
+        // find username from ArrayList of users, remove them
+        // return false if it can't be found/deleted, true otherwise
         return true;
     }
 
