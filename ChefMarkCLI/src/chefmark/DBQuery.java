b95b842782693aac7adacce3f059be3cfcc8ad1b
tcgray <tcg3235@gmail.com>
2023-04-18 11:14:44 -0500

    Updated database

--- ChefMarkCLI/src/chefmark/DBQuery.java
+++ ChefMarkCLI/src/chefmark/DBQuery.java
@@ -3,30 +3,49 @@ package chefmark;
 import java.sql.*;
 
 public class DBQuery {
-    private DBConnection dbconnection;
+    private DBConnection dbConnection;
     private Connection connection;
     private Statement statement;
 
 
-    public void setDBConnection(DBConnection dbconnection)
-    {
-        this.dbconnection = dbconnection;
+    public DBQuery(DBConnection dbConnection) {
+        this.dbConnection = dbConnection;
     }
 
-    public void setConnection(Connection connection)
-    {
-        this.connection = connection;
-    } 
+    public void connect() throws SQLException {
+        String url = dbConnection.getURL();
+        String user = dbConnection.getUsername();
+        String password = dbConnection.getPassword();
 
-    public void setStatement(Statement statement)
-    {
-        this.statement = statement;
+        connection = DriverManager.getConnection(url, user, password);
+
+        statement = connection.createStatement();
     }
-    
+
+    public ResultSet create(User user) {
+        return executeQuery("INSERT INTO USER VALUES(" + user.getUsername() + ", " + user.getPassword() + ", " + user.getEmail());
+    }
+
     public ResultSet read(User user) {
-        
-
+        return executeQuery("SELECT * FROM USER WHERE USERNAME = '" + user.getUsername() + "'");
     }
 
+    public ResultSet update(User user) {
+        return executeQuery("UPDATE USER SET userPassword = '" + user.getPassword() + "', email = '" + user.getEmail() + "' WHERE USERNAME = '" + user.getUsername() + "'");
+    }
+
+    public ResultSet delete(User user) {
+        return executeQuery("DELETE FROM USER WHERE USERNAME = '" + user.getUsername() + "'"); // handles foreign keys
+    }
+
+    private ResultSet executeQuery(String query) throws SQLException {
+        return statement.executeQuery(query);
+    }
+
+    public void disconnect() throws SQLException {
+        statement.close();
+        connection.close();
+    }
 
 }
+
