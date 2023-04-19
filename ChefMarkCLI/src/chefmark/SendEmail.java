a6099a2fbae11f7b2bcfeacddec75272587cd555
mhwilso <mhwilso@ilstu.edu>
2023-04-19 16:01:03 -0500

    removed extra Jar File

--- ChefMarkCLI/src/chefmark/SendEmail.java
+++ ChefMarkCLI/src/chefmark/SendEmail.java
@@ -1,44 +1,72 @@
 package chefmark;
-import java.util.*;
-import javax.mail.*;
-import javax.mail.internet.*;
+import java.util.Properties;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
-public class SendEmail{
-    public void sendMail(String recipientEmail){
-        //Email for test
-        recipientEmail = "maxwilson568@gmail.com";
-        String senderEmail = "ChefMarkIT326@gmail.com";
+public class SendEmail {
 
-        String password = "IT326Chef!";
-        String host = "smtp.gmail.com";
-        String port = "587";
+    public static void sendRecipe(Recipe recipeIn, String recipient) {
+		
+        // Recipient's email ID needs to be mentioned.
+        String to = "maxwilson568@gmail.com";
 
-        Properties props = new Properties();
+        // Sender's email ID needs to be mentioned
+        String from = "ChefMarkIT326@outlook.com";
 
-        props.put("mail.smtp.host", host);
-        props.put("mail.smtp.port", port);
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
+        // Assuming you are sending email from through gmails smtp
+        String host = "smtp-mail.outlook.com";
 
-        Session session = Session.getInstance(props, new Authenticator() {
+        // Get system properties
+        Properties properties = System.getProperties();
+
+        // Setup mail server
+        properties.put("mail.smtp.host", "smtp-mail.outlook.com");
+		properties.put("mail.smtp.port", "587");
+		properties.put("mail.smtp.starttls.enable","true");
+		properties.put("mail.smtp.auth", "true"); 
+
+        // Get the Session object.// and pass username and password
+        Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
+
             protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(senderEmail, password);
+
+                return new PasswordAuthentication("ChefMarkIT326@outlook.com", "IT326Chef!");
+
             }
+
         });
-        
-        try{
-            Message mesage = new MimeMessage(session);
-            message.setFrom(new internetAddress(senderEmail));
-            message.setRecipient(Message.RecipientType.TO, InternetAddress.parse(recipientEmail));
-            message.setSubject("Test email");
-            message.setText("ChefMark test email");
 
+        // Used to debug SMTP issues
+        //session.setDebug(true);
+
+        try {
+            // Create a default MimeMessage object.
+            MimeMessage message = new MimeMessage(session);
+
+            // Set From: header field of the header.
+            message.setFrom(new InternetAddress(from));
+
+            // Set To: header field of the header.
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+            // Set Subject: header field
+            message.setSubject("ChefMark: " + recipeIn.getName() + "recipe!");
+
+            // Now set the actual message
+            message.setText("Recipe goes here i guess");
+
+            // Send message
             Transport.send(message);
+            System.out.println("Recipe shared successfully!");
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
         }
 
-        catch(MessagingException e){
-            System.out.println("Email sending faield");
-        }
-    }
+	}
 }
