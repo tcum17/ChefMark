46a6bf29215d4e29381932fdf6547d5e36cbc87f
tcgray <tcg3235@gmail.com>
2023-05-04 20:07:42 -0500

    Co-authored-by: Wilson, Max <mhwilso@ilstu.edu> Co-authored-by: tcum17 <tcum17@users.noreply.github.com> Co-authored-by: Jake-Noth <Jake-Noth@users.noreply.github.com> Co-authored-by: Msheph1 <Msheph1@users.noreply.github.com>

--- ChefMark/src/com/chefmark/tests/UpdateWeeklyPlanTest.java
+++ ChefMark/src/com/chefmark/tests/UpdateWeeklyPlanTest.java
@@ -14,16 +14,77 @@ import chefmark.*;
 public class UpdateWeeklyPlanTest{
 
     @Test
-    public void updateTestReg() throws SQLException{
+    public void updateTestRecipeAdding() throws SQLException{
         DBConnection dbc = DBConnFactory.getDBConnection(DBConnFactory.DBConnType.MYSQL);
         DBQuery dbq = new MySQLQuery(dbc); // MySQL DBQuery implementation using MySQL Database
         dbq.connect();
+
+        String testName = "test";
         // Create a fake input stream with username and password
         String input = "testuser\nCreate1@1\n";
         InputStream in = new ByteArrayInputStream(input.getBytes());
 
-        WeeklyPlan testPlan = new WeeklyPlan("test plan");
+
         // Create a scanner to read from the fake input stream
         Scanner scanner = new Scanner(in);
+
+        
+
+        UserController uc = new UserController();
+        uc.login(dbq, scanner);
+        WeeklyPlan testPlan = new WeeklyPlan(testName);
+        Recipe testRecipe = new Recipe();
+
+        uc.getUser().addWeeklyPlan(testPlan);
+
+        Boolean result = uc.getUser().getWeeklyPlanByName(testName).addRecipeToWeeklyPlan(testRecipe, "Monday");
+
+        uc.getUser().removeWeeklyPlan(testPlan);
+        assert(result == true);
+
+    }
+
+    @Test
+    public void updateTestRecipeRemoval() throws SQLException{
+        DBConnection dbc = DBConnFactory.getDBConnection(DBConnFactory.DBConnType.MYSQL);
+        DBQuery dbq = new MySQLQuery(dbc); // MySQL DBQuery implementation using MySQL Database
+        dbq.connect();
+
+        String testName = "test";
+        String newTestName = "newName";
+        // Create a fake input stream with username and password
+        String input = "testuser\nCreate1@1\n";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+
+        // Create a scanner to read from the fake input stream
+        Scanner scanner = new Scanner(in);
+
+        //Logging in
+        UserController uc = new UserController();
+        uc.login(dbq, scanner);
+
+        //Creating a weekly plan for testing
+        WeeklyPlan testPlan = new WeeklyPlan(testName);
+
+
+        //Adding plan to be tested
+        uc.getUser().addWeeklyPlan(testPlan);
+
+        //Changing plan name
+        uc.getUser().getWeeklyPlanByName(testName).setName(newTestName);
+
+        Boolean result = false ;
+        if(uc.getUser().getWeeklyPlanByName(newTestName).getName() == newTestName){
+            result = true;
+        }
+
+         uc.getUser().removeWeeklyPlan(uc.getUser().getWeeklyPlanByName(newTestName));
+        
+        assert(result == true);
+    }
+
+    @Test
+    public void updateTestRecipeRenaming() throws SQLException{
+            
     }
 }
