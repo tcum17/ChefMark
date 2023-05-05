42715875b72f6452c124d813daf1780916483409
tcgray <tcg3235@gmail.com>
2023-05-04 19:17:34 -0500

    Co-authored-by: Wilson, Max <mhwilso@ilstu.edu> Co-authored-by: tcum17 <tcum17@users.noreply.github.com> Co-authored-by: Jake-Noth <Jake-Noth@users.noreply.github.com> Co-authored-by: Msheph1 <Msheph1@users.noreply.github.com>

---
+++ ChefMark/src/com/chefmark/tests/UpdateWeeklyPlanTest.java
@@ -0,0 +1,20 @@
+package chefmark.tests;
+
+import org.junit.Test;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+import static org.junit.Assert.*;
+
+import chefmark.*;
+
+public class UpdateWeeklyPlanTest{
+
+    @Test
+    public void updateTestReg(){
+        
+    }
+}
